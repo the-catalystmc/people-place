ab6947c9f3ea7645bf367a2c75baecdcba44bc4c
Rhyine <rhyinestewart@gmail.com>
2021-10-26 11:28:49 -0500

    Fix linter errors

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get users" do
+  test 'should get users' do
     get users_users_url
     assert_response :success
   end
