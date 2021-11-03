82caf640f1e7ae4104e6253ed01127ca509e69f8
Rhyine <rhyinestewart@gmail.com>
2021-11-02 21:49:39 -0500

    Add page for user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:id])
+    @posts = @user.recent_posts
     @limit = params[:limit] unless params[:limit].nil?
   end
 
   def show
     @user = User.find(params[:id])
-    @post = @user.posts.find(params[:id])
+    @post = @user.posts.find(params[:author_id])
   end
 end
