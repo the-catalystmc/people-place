12313b9c255cf6e99340b1736e80235c2fe8a5f7
Rhyine <rhyinestewart@gmail.com>
2021-11-04 15:18:15 -0500

    Add new to post method controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,16 @@ class PostsController < ApplicationController
   end
 
   def show
+    @current_user = current_user
     @user = User.find(params[:id])
     @post = @user.posts.find(params[:post_id])
+  end
+
+  def new
+    @current_user = current_user
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new }
+    end
   end
 end
