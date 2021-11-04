1f2d7df6f50d9f56f52877c8738cae1e1cf8d722
Rhyine <rhyinestewart@gmail.com>
2021-11-04 15:18:39 -0500

    Add and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/users/", to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
+  get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
   get "/users/:id/posts/:post_id", to: "posts#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
