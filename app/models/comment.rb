84edd44d1395fa957ef032161490c8b234ce7131
Rhyine <rhyinestewart@gmail.com>
2021-11-05 10:56:55 -0500

    Add validation for all models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :text, presence: { message: 'Text field cannot be empty.' }
+
   def update_post_comments
     counter = post.comments.count('id')
     post.update(comments_counter: counter)
