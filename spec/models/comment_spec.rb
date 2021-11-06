5fe2271c3592eecce060c2910397560666f94c69
Rhyine <rhyinestewart@gmail.com>
2021-11-05 23:50:43 -0500

    Add model test for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Comment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Comment, type: :model do
+  before :each do
+    @comment = Comment.create(text: 'I am a footballer')
+    @comment.save
+  end
+
+  it 'title should be present' do
+    @comment.text = nil
+    expect(@comment).to_not be_valid
+  end
+end
