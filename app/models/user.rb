84beace3bd4f146c06d08ba7b63406e1f58f9833
Rhyine <rhyinestewart@gmail.com>
2021-11-09 16:23:43 -0500

    Add authorization to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
   has_many :posts, dependent: :delete_all, foreign_key: 'author_id'
   has_many :comments, dependent: :delete_all, foreign_key: 'author_id'
   has_many :likes, dependent: :delete_all, foreign_key: 'author_id'
+
+  # attr_accessible :name , :email
+
+  # # User::Roles
+  # # The available roles
+  # Roles = [ :admin , :default ]
+
+  # def is?( requested_role )
+  #   self.role == requested_role.to_s
+  # end
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
@@ -21,5 +30,9 @@ class User < ApplicationRecord
 
   def posts_counter
     posts.count
+  end
+
+  def admin?
+    role == 'Admin'
   end
 end
