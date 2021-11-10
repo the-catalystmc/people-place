2e93928ccc3d91adf2929a3f28472d20c8c9aeec
Rhyine <rhyinestewart@gmail.com>
2021-11-09 19:18:08 -0500

    Add option to delete comments and likes with post

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   post '/users/:id/posts/:post_id/comments/new', to: 'comments#create' # create
   post "/users/:id/posts/:post_id/like", to: "likes#like", as: "user_create_post_like"
   delete "/users/:id/posts/:post_id", to: "posts#destroy", as: "destroy_post"
+  delete "/users/:id/posts/:post_id/comments/:comment_id", to: "comments#destroy", as: "destroy_comments"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(
+    email: "admin@admin.com",
+    password: "admin123",
+    password_confirmation: "admin123",
+    name: 'Admin'
+    role: 'Admin'
+    )
+    user.skip_confirmation!
+    user.save! 
