85d3544968508a962a715bae694c4486a8c54fe5
Rhyine <rhyinestewart@gmail.com>
2021-11-11 18:10:51 -0500

    Setup endpoint and json return

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::CommentsController < ApiController
+  before_action :set_comments
+
+  def show; end
+
+  def index
+    @comments.each do |comment|
+      render json: {
+        id: comment.id,
+        text: comment.text,
+        author_id: comment.author_id,
+        post_id: comment.post_id
+      }
+    end
+  end
+
+  private
+
+  def set_comments
+    @post = Post.find(params[:post_id])
+    @comments = @post.recent_comments(100)
+  end
+end
