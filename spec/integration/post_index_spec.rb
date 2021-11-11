efc3aa24e676b2386e2782961c8d91e1d95f3f43
Rhyine <rhyinestewart@gmail.com>
2021-11-10 19:01:35 -0500

    Update describe

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User Index Page', type: :system do
+RSpec.describe 'Post Index Page', type: :system do
   before(:all) do
     @admin = User.create(name: 'Admin', photo: 'photo',
                          bio: 'I am a software developer.',
