0c2cab1d2691927cc2f4ca19f45f2133b4622524
Rhyine <rhyinestewart@gmail.com>
2021-11-11 15:59:26 -0500

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users do
+      resources :posts do
+        resources :comments
+      end
+    end
+  end
   devise_for :users
   root "users#index"
   get "/users/", to: "users#index", as: 'users'
-  get "/users/:id", to: "users#show"
-  get "/users/:id/posts", to: "posts#index", as: 'user_post'
-  get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
-  get "/users/:id/posts/:post_id", to: "posts#show", as: 'post_comments'
-  get "/users/:id/posts/:post_id/comments/new", to: "comments#new", as: "new_comment"
+  get "/users/:user_id", to: "users#show"
+  get "/users/:user_id/posts", to: "posts#index", as: 'user_post'
+  get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post' # new
+  get "/users/:user_id/posts/:post_id", to: "posts#show", as: 'post_comments'
+  get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new", as: "new_comment"
 
 
-  post '/users/:id/posts/new', to: 'posts#create' # create
-  post '/users/:id/posts/:post_id/comments/new', to: 'comments#create' # create
-  post "/users/:id/posts/:post_id/like", to: "likes#like", as: "user_create_post_like"
-  delete "/users/:id/posts/:post_id", to: "posts#destroy", as: "destroy_post"
-  delete "/users/:id/posts/:post_id/comments/:comment_id", to: "comments#destroy", as: "destroy_comments"
+  post '/users/:user_id/posts/new', to: 'posts#create' # create
+  post '/users/:user_id/posts/:post_id/comments/new', to: 'comments#create' # create
+  post "/users/:user_id/posts/:post_id/like", to: "likes#like", as: "user_create_post_like"
+  delete "/users/:user_id/posts/:post_id", to: "posts#destroy", as: "destroy_post"
+  delete "/users/:user_id/posts/:post_id/comments/:id", to: "comments#destroy", as: "destroy_comments"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
