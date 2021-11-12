9f93002c6e984523f0f0dfc69ac013333bb49b98
Rhyine <rhyinestewart@gmail.com>
2021-11-12 11:56:44 -0500

    Add needed protection files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,33 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, unless: :json_request
-  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
-
   include Response
   include ExceptionHandler
-  
+
+  protect_from_forgery if: :json_request # return null session when API call
+  protect_from_forgery with: :exception, unless: :json_request
+
+  before_action :authorize_request, if: :json_request
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
   protected
-  
-  def json_request
-      request.format.json?
-  end
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :email, :password)
+    end
+
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :bio, :email, :password, :current_password)
     end
+  end
+
+  private
+
+  def json_request
+    request.format.json?
+  end
+
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
   end
 end
