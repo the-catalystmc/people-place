03f008c6901fbf16202e7ff274f926d12a8490c3
Rhyine <rhyinestewart@gmail.com>
2021-11-12 10:35:44 -0500

    Update response and exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+  protect_from_forgery with: :exception, unless: :json_request
   before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
 
+  include Response
+  include ExceptionHandler
+  
   protected
+  
+  def json_request
+      request.format.json?
+  end
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
