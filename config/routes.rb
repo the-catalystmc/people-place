48e334a5b835d194512e5c574ed91a6174aa832b
Rhyine <rhyinestewart@gmail.com>
2021-11-12 10:38:02 -0500

    Add jwt and associated helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
+    post 'login', to: 'authentication#authenticate'
+    post 'signup', to: 'users#create' 
     resources :allposts
     resources :users do
       resources :posts do
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
       end
     end
   end
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   root "users#index"
   get "/users/", to: "users#index", as: 'users'
   get "/users/:user_id", to: "users#show"
@@ -22,6 +24,5 @@ Rails.application.routes.draw do
   post "/users/:user_id/posts/:post_id/like", to: "likes#like", as: "user_create_post_like"
   delete "/users/:user_id/posts/:post_id", to: "posts#destroy", as: "destroy_post"
   delete "/users/:user_id/posts/:post_id/comments/:id", to: "comments#destroy", as: "destroy_comments"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
